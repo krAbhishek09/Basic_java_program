e2e343e729ccf469b6cb8e95065dea5e820c2577
krAbhishek09 <abhishekbhagat6654@gmail.com>
2024-10-27 20:50:39 +0530

    small changes

---
+++ Salr.java
@@ -0,0 +1,17 @@
+
+public class Salr {
+
+    public static void main(String[] args) {
+
+        // syntax of it statements:
+        int salary = 3433;
+        if (salary > 1000) {
+            salary = salary + 200;
+        } else {
+            salary = salary + 10;
+        }
+        System.out.println(salary);
+
+    }
+
+}
